4ead909255086886b4a2ec2a686cc85cbc074f12
Cristian-10101 <cristian.av.1997@gmail.com>
2021-06-17 17:57:33 -0600

    Inicio de Proyecto

---
+++ FamgarSar/FamgarSar/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FamgarSar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
